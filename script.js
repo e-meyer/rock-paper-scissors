dc38e29083904b63ef6f7b6eb414f6d00cc9ae70
e-meyer <meyeredy20@gmail.com>
2022-07-19 21:19:33 -0300

    Split functions for better looking code, implement play again display and functionality

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const buttons = Array.from(document.getElementsByClassName('button'))
 const buttons_pc = Array.from(document.querySelectorAll('#computer'))
+let playAgain = document.querySelector('.play-again')
+let endResultOutput = document.querySelector('#output')
 let versus = document.querySelector('.versus')
-let changeScore = document.querySelectorAll('.player-points, .computer-points')             // retusn a nodelist of 2 position which will be used to 
+let arrScores = document.querySelectorAll('.player-points, .computer-points')             // retusn a nodelist of 2 position which will be used to 
                                                                                             // display the score of the computer and the player
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
@@ -14,21 +16,23 @@ buttons.forEach((button) => {
         let playerSelection = e.currentTarget.id
         let computerSelection = computerPlay()
 
-
         addStyle(computerSelection);
 
         let finalScore = checkStatus(playerSelection, computerSelection)
 
         if (finalScore == 3) { 
-            changeScore[1].innerText++;                                                     // if the checkstatus function returns 3, then the points will
-                                                                                            // go to the player
-            versus.innerText = `You won! ${capitalize(playerSelection)} wins over ${computerSelection}`
+            arrScores[1].innerText++                                                      // if the checkstatus function returns 3, then the points will
+                                                                                            // go to the player                                        
+            versus.innerText = `You won! ${capitalizeString(playerSelection)} beats ${computerSelection}`
             versus.style.color = 'rgb(222, 96, 231)'
         }
         else if (finalScore == 2){
-            changeScore[0].innerText++;                                                     // if it returns 2 then it will be counted for the computer
+            arrScores[0].innerText++                                                      // if it returns 2 then it will be counted for the computer
 
-            versus.innerText = `You lost! ${computerSelection} wins over ${capitalize(playerSelection)}`
+            versus.innerText = `You lost! ${computerSelection} beats ${capitalizeString(playerSelection)}`
+            versus.style.color = 'rgba(0,0,0,0.4)'
+        } else {
+            versus.innerText = `Draw!`
             versus.style.color = 'rgba(0,0,0,0.4)'
         }
 
@@ -37,17 +41,23 @@ buttons.forEach((button) => {
                 button.style.cursor = 'pointer'; 
                 versus.innerText = 'x'
                 versus.style.color = 'rgba(0,0,0,0.4)'
-            })}, 2500);
+            })}, 2000);
         
+                    
+        if(checkWinner(arrScores[0].innerText, arrScores[1].innerText)){
+            if(arrScores[0].innerText >= 5) displayPlayAgain('lost')
+            
+            else displayPlayAgain('won')
+        }
+
     })
     
 })
 
-function capitalize(str) {
+function capitalizeString(str) {
     str = str.toLowerCase();
-    let firstLetter = str.toUpperCase()[0];
-    str = str.replace(str.charAt(0), '');
-    return firstLetter + str;
+    str = str.replace(str.charAt(0), str.toUpperCase()[0]);
+    return str;
 }
 
 let addStyle = (selection) => {
@@ -55,15 +65,23 @@ let addStyle = (selection) => {
     sizeIncreaser.classList.add('toggle')                                                   // using a css class called toggle, which has a style
 }                                                                                           // in style.css folder
 
-buttons_pc.forEach(buttonpc => buttonpc.addEventListener('transitionend', (e) => {          // this is used to listen when the events of transition
-    e.currentTarget.classList.remove('toggle');                                             // of the previous function ends, then the class toggle
-}))                                                                                         // previously applied will be removed and so the style
-
 function computerPlay() {   
     let arrOptions = ['Rock', 'Paper', 'Scissors']                                          
     let randomIndex = Math.floor(Math.random() * (2 + 1))                                   // gets a random number ranging from 0 to 2 and returns
                                                                                             // it so the function computerPlay can return a random
     return arrOptions[randomIndex]                                                          // chose between rock, paper and scissors
+}
+
+function checkWinner(computerPoints, playerPoints){
+    if (playerPoints >= 5 || computerPoints >= 5) return true
+
+    return false
+}   
+
+function displayPlayAgain(finalResult) {
+    endResultOutput.innerText = finalResult
+    playAgain.style.display = 'flex'
+    document.querySelector('.play-again .button').addEventListener('click', () => location.reload())
 }
 
 function checkStatus(playerSelection, computerSelection) {                                  
@@ -93,3 +111,7 @@ function checkStatus(playerSelection, computerSelection) {
             break
     }
 }
+
+buttons_pc.forEach(buttonpc => buttonpc.addEventListener('transitionend', (e) => {          // this is used to listen when the events of transition
+    e.currentTarget.classList.remove('toggle');                                             // of the previous function ends, then the class toggle
+}))                                                                                         // previously applied will be removed and so the style
